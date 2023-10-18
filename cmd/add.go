acaa6d44eb9ebebe64cc61eebe1a0d79fe881ba6
t.vladimir <vovatatosyan2000@mail.ru>
2023-10-18 18:03:56 +0300

    Added priority

--- cmd/add.go
+++ cmd/add.go
@@ -10,17 +10,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var priority int
+
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new todo",
 	Long:  `Add will create a new todo item to the list`,
 	Run: func(cmd *cobra.Command, args []string) {
-		items := []todo.Item{}
-		for _, arg := range args {
-			items = append(items, todo.Item{Text: arg})
+		items, err := todo.ReadItems(dataFile)
+		if err != nil {
+			fmt.Errorf("%v", err)
 		}
-		err := todo.SaveItems(dataFile, items)
+
+		for _, arg := range args {
+			item := todo.Item{Text: arg}
+			item.SetPriority(priority)
+			items = append(items, item)
+		}
+		err = todo.SaveItems(dataFile, items)
 		if err != nil {
 			fmt.Errorf("%v", err)
 		}
@@ -30,6 +38,7 @@ var addCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(addCmd)
 
+	addCmd.Flags().IntVarP(&priority, "priority", "p", 2, "Priority:1,2,3")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
--- cmd/list.go
+++ cmd/list.go
@@ -14,13 +14,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List your todos",
+	Long:  `List the list of your todos`,
 	Run: func(cmd *cobra.Command, args []string) {
 		items, err := todo.ReadItems(dataFile)
 		if err != nil {
