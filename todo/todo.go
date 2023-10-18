bcaaefd27eb1dd02ea938620da5b10b4c7796ef3
t.vladimir <vovatatosyan2000@mail.ru>
2023-10-18 18:24:10 +0300

    Added output with prioriting, added sorting

--- todo/todo.go
+++ todo/todo.go
@@ -67,3 +67,22 @@ func ReadItems(filename string) ([]Item, error) {
 
 	return items, nil
 }
+
+// ByPri implements sort interface for []Item based on
+// the priority & position field.
+type ByPri []Item
+
+func (s ByPri) Len() int {
+	return len(s)
+}
+
+func (s ByPri) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s ByPri) Less(i, j int) bool {
+	if s[i].Priority == s[j].Priority {
+		return s[i].position < s[j].position
+	}
+	return s[i].Priority < s[j].Priority
+}
